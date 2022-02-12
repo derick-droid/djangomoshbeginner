c83ef390aa8f20c14213f2f6cc47d07850b71887
derick-droid <derrickokinda9@gmail.com>
2022-02-12 09:51:52 +0300

    django beginner to advance

--- start/playground/views.py
+++ start/playground/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
+    x = 1
+    y = 2
     return HttpResponse("hello world palms")
 
 def hello(request):
